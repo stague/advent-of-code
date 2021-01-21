1b2ea2496406a319e3dfddff368ac88d05e8a919
Mike Emery <memery@figure.com>
2021-01-21 16:41:08 -0700

    2015 day 2

--- advent/src/main/kotlin/advent/PuzzleRunner.kt
+++ advent/src/main/kotlin/advent/PuzzleRunner.kt
@@ -1,9 +1,9 @@
 package advent
 
-import advent.year2020.Dec25
+import advent.year2015.Dec2Y2015
 
 fun main() {
-    val output = Dec25().run()
+    val output = Dec2Y2015().run()
     println("--------------------------------------------------------------------------------")
     println(output)
 }
