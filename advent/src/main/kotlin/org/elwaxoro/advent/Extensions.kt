d4b24e1efdae3967f845307cadfc8e164ba33e53
Mike Emery <memery@figure.com>
2021-12-03 12:54:42 -0700

    2021-12-03

--- advent/src/main/kotlin/org/elwaxoro/advent/Extensions.kt
+++ advent/src/main/kotlin/org/elwaxoro/advent/Extensions.kt
@@ -1,3 +1,30 @@
 package org.elwaxoro.advent
 
 fun ByteArray.toHexString() = joinToString("") { (0xFF and it.toInt()).toString(16).padStart(2, '0') }
+
+/**
+ * Split a string into a list of integers
+ * Ex: "01234" becomes [0, 1, 2, 3, 4]
+ */
+fun String.splitToInt(): List<Int> = map(Character::getNumericValue)
+
+/**
+ * Replace matching values
+ */
+fun List<Int>.replace(oldInt: Int, newInt: Int): List<Int> = map { it.takeUnless { it == oldInt } ?: newInt }
+
+/**
+ * Takes a list of 1s and 0s and turns it into a single integer
+ */
+fun List<Int>.toBinaryInt(): Int = joinToString("").toInt(2)
+
+/**
+ * 0s become 1s, 1s become 0s
+ */
+fun List<Int>.bitFlip(): List<Int> = map {
+    when (it) {
+        1 -> 0
+        0 -> 1
+        else -> throw IllegalStateException("can't bit flip $it")
+    }
+}
---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec03.kt
@@ -0,0 +1,47 @@
+package org.elwaxoro.advent.y2021
+
+import org.elwaxoro.advent.PuzzleDayTester
+import org.elwaxoro.advent.bitFlip
+import org.elwaxoro.advent.replace
+import org.elwaxoro.advent.splitToInt
+import org.elwaxoro.advent.toBinaryInt
+
+/**
+ * Binary Diagnostic
+ */
+class Dec03 : PuzzleDayTester(3, 2021) {
+
+    /**
+     * Swap 0s for -1s then just zip all the lists together into a list of sums
+     * If it's negative that's a 0 if its positive that's a 1
+     */
+    override fun puzzle1(): Any = load().map { it.splitToInt().replace(0, -1) }.let { readings ->
+        readings.fold(List(readings.first().size) { 0 }) { acc, reading ->
+            acc.zip(reading, Int::plus)
+        }
+    }.map { num -> 1.takeIf { num > 0 } ?: 0 }.let {
+        it.toBinaryInt() * it.bitFlip().toBinaryInt()
+    }
+
+    override fun puzzle2(): Any = load().let {
+        it.seekReading(true) * it.seekReading(false)
+    }
+
+    private fun List<String>.seekReading(keepMostCommon: Boolean): Int = seekReading(keepMostCommon, 0, this).single().toInt(2)
+
+    /**
+     * Partition the lists based on 0 or 1 at idx
+     * Use keepMostCommon to decide which side to keep for next recursion
+     */
+    private tailrec fun seekReading(keepMostCommon: Boolean, idx: Int, readings: List<String>): List<String> =
+        if (readings.size == 1) {
+            readings
+        } else {
+            seekReading(keepMostCommon, idx + 1,
+                readings.partition { it[idx] == '0' }.let { (zeros, ones) ->
+                    ones.takeIf { (keepMostCommon && ones.size >= zeros.size) || (!keepMostCommon && ones.size < zeros.size) }
+                        ?: zeros
+                }
+            )
+        }
+}
