666bfe8ef0dde1f61729aac563848f53278b5d96
Mike Emery <memery@figure.com>
2021-12-06 10:56:55 -0700

    2021-12-06

--- advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec06.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec06.kt
@@ -14,7 +14,7 @@ class Dec06 : PuzzleDayTester(6, 2021) {
         // Lanternfish spawn on an 8 day cycle plus one extra bucket for newbies
         MutableList(9) { 0L }.also { fishes ->
             // load the starting fish in there
-            load(delimiter = ",").map { it.toInt() }.groupingBy { it }.eachCount().map { (t, u) -> fishes[t] = u.toLong() }
+            load(delimiter = ",").map { it.toInt() }.groupingBy { it }.eachCount().map { (idx, count) -> fishes[idx] = count.toLong() }
         }.let { fishes ->
             (0 until days).forEach { day ->
                 val spawnIdx = day % 8 // ignore last bucket, that's the nursery and it never spawns
