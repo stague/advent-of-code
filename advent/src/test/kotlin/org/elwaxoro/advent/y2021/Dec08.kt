9e51463d94a2426243e1a3797e0a0006dbf81d13
Mike Emery <memery@figure.com>
2021-12-08 10:09:10 -0700

    2021-12-08

--- advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec08.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec08.kt
@@ -11,7 +11,8 @@ class Dec08 : PuzzleDayTester(8, 2021) {
      * Only care about the right side of the pipe
      * Only care about unique length digits
      */
-    override fun puzzle1(): Any = load().map { it.split(" | ").let { it[0] to it[1] } }.sumOf { it.second.split(" ").filter { it.length in listOf(2, 3, 4, 7) }.size }
+    override fun puzzle1(): Any = load().map { it.split(" | ").let { it[0] to it[1] } }
+        .sumOf { it.second.split(" ").filter { it.length in listOf(2, 3, 4, 7) }.size }
 
     /**
      * 1, 4, 7, 8 all have unique segment counts
@@ -23,19 +24,20 @@ class Dec08 : PuzzleDayTester(8, 2021) {
      * sum of remaining for each mask = 10
      * Each 5 segment digit masks to a different sum, same with 6 segment digits (note 5 and 6 overlap so doing separately)
      */
-    override fun puzzle2(): Any = load().map { it.replace(" |", "").split(" ") }.map { it to it.buildMasks() }.map { (digits, masks) ->
-        digits.map { digit ->
-            when(digit.length) {
-                2 -> 1
-                3 -> 7
-                4 -> 4
-                7 -> 8
-                5 -> masks.apply(digit).to5SegmentDigit() //2, 3, 5
-                6 -> masks.apply(digit).to6SegmentDigit() //6, 9, 0
-                else -> throw IllegalStateException("Unknown digit with unsupported lit segments: $digit")
-            }
-        }.takeLast(4).joinToString("").toInt()
-    }.sum()
+    override fun puzzle2(): Any =
+        load().map { it.replace(" |", "").split(" ") }.map { it to it.buildMasks() }.map { (digits, masks) ->
+            digits.map { digit ->
+                when (digit.length) {
+                    2 -> 1
+                    3 -> 7
+                    4 -> 4
+                    7 -> 8
+                    5 -> masks.apply(digit).to5SegmentDigit() //2, 3, 5
+                    6 -> masks.apply(digit).to6SegmentDigit() //6, 9, 0
+                    else -> throw IllegalStateException("Unknown digit with unsupported lit segments: $digit")
+                }
+            }.takeLast(4).joinToString("").toInt()
+        }.sum()
 
     private fun List<String>.buildMasks(): List<Regex> = listOf(
         Regex("[${first { it.length == 2 }}]+"), // 1
@@ -46,7 +48,7 @@ class Dec08 : PuzzleDayTester(8, 2021) {
     private fun List<Regex>.apply(digit: String) = fold(0) { acc, mask -> acc + digit.replace(mask, "").length }
 
     private fun Int.to5SegmentDigit(): Int =
-        when(this) {
+        when (this) {
             10 -> 2
             7 -> 3
             9 -> 5
@@ -54,7 +56,7 @@ class Dec08 : PuzzleDayTester(8, 2021) {
         }
 
     private fun Int.to6SegmentDigit(): Int =
-        when(this) {
+        when (this) {
             12 -> 6
             9 -> 9
             10 -> 0
