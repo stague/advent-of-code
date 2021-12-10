a45c8b239bd42031e2f66ee26086a032d4e01b89
Mike Emery <memery@figure.com>
2021-12-10 13:22:47 -0700

    2015-12-10

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2015/Dec10.kt
@@ -0,0 +1,33 @@
+package org.elwaxoro.advent.y2015
+
+import org.elwaxoro.advent.PuzzleDayTester
+
+class Dec10 : PuzzleDayTester(10, 2015) {
+
+    override fun puzzle1(): Any = (1..40).fold("3113322113") { acc, _ ->
+        acc.lookAndSay()
+    }.length
+
+    override fun puzzle2(): Any = (1..50).fold("3113322113") { acc, _ ->
+        acc.lookAndSay()
+    }.length
+
+    private fun String.lookAndSay(): String {
+        var current = this[0]
+        var counter = 0
+        val newString = mutableListOf<Any>()
+        forEach { i ->
+            if (i == current) {
+                counter++
+            } else {
+                newString.add(counter)
+                newString.add(current)
+                current = i
+                counter = 1
+            }
+        }
+        newString.add(counter)
+        newString.add(current)
+        return newString.joinToString("")
+    }
+}
