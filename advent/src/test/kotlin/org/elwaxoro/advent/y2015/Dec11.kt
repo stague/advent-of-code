46c7dc3911004b1607caa129f1c5a331cbfd50ce
Mike Emery <memery@figure.com>
2021-12-10 18:18:48 -0700

    2015-12-11

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2015/Dec11.kt
@@ -0,0 +1,62 @@
+package org.elwaxoro.advent.y2015
+
+import org.elwaxoro.advent.PuzzleDayTester
+
+/**
+ * Corporate Policy
+ */
+class Dec11 : PuzzleDayTester(11, 2015) {
+
+    override fun puzzle1(): Any {
+        val password = "hepxcrrq".toMutableList()
+        findNextPassword(password)
+        return password.joinToString("")
+    }
+
+    override fun puzzle2(): Any {
+        val password = "hepxcrrq".toMutableList()
+        findNextPassword(password)
+        findNextPassword(password)
+        return password.joinToString("")
+    }
+
+    private fun findNextPassword(input: MutableList<Char>) {
+        var ctr = 0L
+        input.increment()
+        while(!input.isValid()) {
+            ctr++
+            input.increment()
+        }
+    }
+
+    private fun MutableList<Char>.isValid(): Boolean =
+        noIllegalChars() && containsTwoPairs() && containsStraight()
+
+    private fun MutableList<Char>.containsTwoPairs(): Boolean {
+        var lastPairIdx = -1
+        var pairs = 0
+        (0..size-2).forEach { idx ->
+            if(lastPairIdx < idx && this[idx] == this[idx+1]) {
+                pairs++
+                lastPairIdx = idx+1
+            }
+        }
+        return pairs > 1
+    }
+
+    private fun MutableList<Char>.noIllegalChars(): Boolean =
+        none { it == 'i' || it == 'o' || it == 'l' }
+
+    private fun MutableList<Char>.containsStraight(): Boolean =
+        (0..size - 3).any { this[it] + 1 == this[it + 1] && this[it] + 2 == this[it + 2] }
+
+    private fun MutableList<Char>.increment() {
+        this[size - 1] = this[size - 1] + 1
+        (size - 1).downTo(0).forEach { idx ->
+            if (this[idx] > 'z' && idx > 0) {
+                this[idx - 1] = this[idx - 1] + 1
+            }
+        }
+        this.replaceAll { c -> 'a'.takeIf { c > 'z' } ?: c }
+    }
+}
