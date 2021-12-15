b0e93d3d335d6d9064a42bb80eda9e45316203ff
Mike Emery <memery@figure.com>
2021-12-15 12:38:55 -0700

    dijkstra

--- advent/src/main/kotlin/org/elwaxoro/advent/Graphs.kt
+++ advent/src/main/kotlin/org/elwaxoro/advent/Graphs.kt
@@ -1,17 +1,56 @@
 package org.elwaxoro.advent
 
-data class Node(val name: String, val canVisit: Boolean = true, val canLeave: Boolean = true, val canRevisit: Boolean = false) {
+data class Node(
+    val name: String,
+    val canVisit: Boolean = true,
+    val canLeave: Boolean = true,
+    val canRevisit: Boolean = false
+) {
 
     val edges = mutableMapOf<Node, Int>()
     val nodes = mutableListOf<Node>()
 
+    // helpers for dijkstra
+    var shortestPathToSource = listOf<Node>()
+    var shortestDistance: Int = Int.MAX_VALUE
+
+    override fun toString(): String = name
+
     fun cost(node: Node): Int = edges[node]!!
 
     fun addEdge(node: Node, cost: Int = 0) {
-        if(!edges.contains(node)) {
+        if (!edges.contains(node)) {
             nodes.add(node)
         }
         edges[node] = cost
+    }
+
+    /**
+     * Dijkstra it up!
+     * This call populates this node and all connected nodes with the shortest path from this node to all other connected nodes
+     */
+    fun dijkstra() {
+        shortestDistance = 0
+        val settled: MutableSet<Node> = mutableSetOf()
+        val unsettled: MutableSet<Node> = mutableSetOf()
+        unsettled.add(this)
+
+        while (unsettled.isNotEmpty()) {
+            val current = unsettled.minByOrNull { it.shortestDistance }!!
+            unsettled.remove(current)
+            current.nodes.filterNot { settled.contains(it) }.forEach { neighbor ->
+                neighbor.calculateMinimumDistance(current)
+                unsettled.add(neighbor)
+            }
+            settled.add(current)
+        }
+    }
+
+    private fun calculateMinimumDistance(source: Node) {
+        (source.shortestDistance + cost(source)).takeIf { it < shortestDistance }?.let { newShorterDistance ->
+            shortestDistance = newShorterDistance
+            shortestPathToSource = source.shortestPathToSource + source
+        }
     }
 }
 
@@ -20,7 +59,7 @@ data class Node(val name: String, val canVisit: Boolean = true, val canLeave: Bo
  */
 fun List<Node>.addNode(node: Node): List<Node> = map {
     it.also {
-        if(node.edges.contains(it)) {
+        if (node.edges.contains(it)) {
             it.addEdge(node, node.cost(it))
         }
     }
@@ -33,13 +72,18 @@ fun maxPath(nodes: List<List<Node>>): List<Node> = nodes.maxByOrNull { it.cost()
 fun List<Node>.cost(): Int = zipWithNext { a, b -> a.cost(b) }.sum()
 
 /**
- * Assumes a fully connected bidirectional graph
+ * Assumes a fully connected bidirectional graph, find the best path that includes ALL nodes
  */
-fun findBestPath(nodes: List<Node>, connectLoop: Boolean, bestPathFinder: (nodes: List<List<Node>>) -> List<Node>, costFunction: (nodes: List<List<Node>>) -> Int): Int =
+fun findBestPath(
+    nodes: List<Node>,
+    connectLoop: Boolean,
+    bestPathFinder: (nodes: List<List<Node>>) -> List<Node>,
+    costFunction: (nodes: List<List<Node>>) -> Int
+): Int =
     costFunction(nodes.map {
         visitAllNodes(listOf(it), nodes.minus(it), bestPathFinder)
     }.map {
-        if(connectLoop) {
+        if (connectLoop) {
             it.plus(it.first())
         } else {
             it
@@ -49,7 +93,11 @@ fun findBestPath(nodes: List<Node>, connectLoop: Boolean, bestPathFinder: (nodes
 /**
  * Assumes a fully connected bidirectional graph
  */
-fun visitAllNodes(visited: List<Node>, remaining: List<Node>, bestPathFinder: (nodes: List<List<Node>>) -> List<Node>): List<Node> =
+fun visitAllNodes(
+    visited: List<Node>,
+    remaining: List<Node>,
+    bestPathFinder: (nodes: List<List<Node>>) -> List<Node>
+): List<Node> =
     if (remaining.isEmpty()) {
         visited
     } else {
