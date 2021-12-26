017912bea4d7019d430e7da2a5d9012107669939
Mike Emery <memery@figure.com>
2021-12-26 08:46:35 -0700

    2021-12-23

---
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec23.kt
@@ -0,0 +1,11 @@
+package org.elwaxoro.advent.y2021
+
+import org.elwaxoro.advent.PuzzleDayTester
+
+/**
+ * Amphipod
+ */
+class Dec23: PuzzleDayTester(23, 2021) {
+    override fun puzzle1(): Any = "I did this by hand :("
+    override fun puzzle2(): Any = "I did this by hand too :)"
+}
