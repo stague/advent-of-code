8a20227f50138dc44d21a91807d8ac1f7945b276
Mike Emery <memery@figure.com>
2021-12-28 12:53:38 -0700

    2021-12-25

--- advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec25.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec25.kt
@@ -14,10 +14,12 @@ class Dec25 : PuzzleDayTester(25, 2021) {
             val newGrid = List(grid.size) { MutableList(grid.first().size) { '.' } }
             var moved = 0
 
+            // move it to the left to the left to the left
             grid.forEachIndexed { y, row ->
                 row.forEachIndexed { x, c ->
                     if (c == '>') {
                         val moveX = (x + 1) % row.size
+                        // only care about what was there before, since everything moves simultaneously
                         if (row[moveX] == '.') {
                             newGrid[y][moveX] = '>'
                             moved++
@@ -28,10 +30,12 @@ class Dec25 : PuzzleDayTester(25, 2021) {
                 }
             }
 
+            // now get down with it
             grid.forEachIndexed { y, row ->
                 row.forEachIndexed { x, c ->
                     if (c == 'v') {
                         val moveY = (y + 1) % grid.size
+                        // need to check what was there before for 'v' and that the new space is still open from the left movers
                         if (grid[moveY][x] != 'v' && newGrid[moveY][x] == '.') {
                             newGrid[moveY][x] = 'v'
                             moved++
@@ -46,7 +50,7 @@ class Dec25 : PuzzleDayTester(25, 2021) {
             rep++
         } while (moved > 0)
         rep
-    } == 389
+    }
 
     override fun puzzle2(): Any = "There is no part 2! Merry Christmas!"
 }
