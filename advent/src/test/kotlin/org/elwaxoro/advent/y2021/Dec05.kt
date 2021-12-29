8a9da73b8c2a3f6c10036208dd903e1f762b4b39
Mike Emery <memery@figure.com>
2021-12-29 10:55:05 -0700

    2021-12-05 cleanup

--- advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec05.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2021/Dec05.kt
@@ -15,18 +15,4 @@ class Dec05 : PuzzleDayTester(5, 2021) {
     private fun List<List<Coord>>.enumerateAndCount() = flatMap { it.first().enumerateLine(it.last()) }.groupingBy { it }.eachCount().filter { it.value > 1 }.size
 
     private fun parse(): List<List<Coord>> = load().map { it.split(" -> ").map(Coord::parse) }
-
-// Copied from Extensions.kt and Coordinates.kt
-//    fun Coord.enumerateLine(toCoord: Coord): List<Coord> =
-//        (abs(x -toCoord.x) + 1 to abs(y - toCoord.y) + 1).let { (dx, dy) ->
-//            x.toward(toCoord.x).padTo(dy).zip(y.toward(toCoord.y).padTo(dx)).map { (x, y) ->
-//                Coord(x, y)
-//            }
-//        }
-//
-//    fun Int.toward(to: Int): IntProgression = IntProgression.fromClosedRange(this, to, 1.takeIf { this <= to } ?: -1)
-//
-//    fun IntProgression.padTo(newSize: Int): List<Int> = toList().padTo(newSize)
-//
-//    fun <T> List<T>.padTo(newSize: Int): List<T> = takeIf { size >= newSize } ?: plus(List(newSize - size) { last() })
 }
