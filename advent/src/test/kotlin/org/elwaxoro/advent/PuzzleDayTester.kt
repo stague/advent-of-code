f85845b40436d26f6b847a5e27573132e602284c
Emery <memery@figure.com>
2022-09-28 15:58:13 -0600

    add some timing to the print

--- advent/src/test/kotlin/org/elwaxoro/advent/PuzzleDayTester.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/PuzzleDayTester.kt
@@ -1,23 +1,23 @@
 package org.elwaxoro.advent
 
+import java.time.OffsetDateTime
+import kotlin.system.measureTimeMillis
 import kotlin.test.Test
 import kotlin.test.assertNotEquals
 
 abstract class PuzzleDayTester(day: Int, year: Int): PuzzleDay(day, year) {
 
     @Test
-    open fun testPuzzle1() {
-        puzzle1().let {
-            println("Puzzle 1: $it")
-            assertNotEquals("TODO", it, "Puzzle 1 not implemented")
-        }
-    }
+    open fun testPuzzle1() = testPuzzle("Puzzle 1", ::puzzle1)
 
     @Test
-    open fun testPuzzle2() {
-        puzzle2().let {
-            println("Puzzle 2: $it")
-            assertNotEquals("TODO", it, "Puzzle 2 not implemented")
+    open fun testPuzzle2() = testPuzzle("Puzzle 2", ::puzzle2)
+
+    private fun testPuzzle(name: String, puzzle: () -> Any) {
+        val start = System.currentTimeMillis()
+        puzzle().let {
+            println("$name: $it [${System.currentTimeMillis()-start}ms]")
+            assertNotEquals("TODO", it, "$name not implemented")
         }
     }
 }
