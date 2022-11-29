49330e788602d6866ced94760359e1e50fee74e2
Emery <memery@figure.com>
2022-11-29 13:43:04 -0700

    upgrade gradle, kotlin

--- advent/build.gradle.kts
+++ advent/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.6.0"
-    kotlin("plugin.serialization") version "1.6.0"
+    id("org.jetbrains.kotlin.jvm") version "1.7.21"
+    kotlin("plugin.serialization") version "1.7.21"
     application
 }
 
@@ -11,13 +11,13 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
     implementation("com.google.guava:guava:30.1.1-jre")
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
