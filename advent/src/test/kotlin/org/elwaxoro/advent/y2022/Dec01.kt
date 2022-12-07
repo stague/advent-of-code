6c04bb54da4ca1aef2dd72928cfb3eb72c9314ea
Steve Tague <stevetague@f0971.lan>
2022-12-07 14:04:24 -0700

    Bye test files

--- advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
+++ advent/src/test/kotlin/org/elwaxoro/advent/y2022/Dec01.kt
@@ -3,10 +3,10 @@ package org.elwaxoro.advent.y2022
 import org.elwaxoro.advent.PuzzleDayTester
 
 class Dec01: PuzzleDayTester(1, 2022) {
-//    override val testRuns = listOf(
-//            TestRun(1, 1000, 1000),
-//            TestRun(null, 11223, 11223)
-//    )
+    override val testRuns = listOf(
+            TestRun(1, 1000, 1000),
+            TestRun(null, 11223, 11223)
+    )
 
     override fun part1(testFileSuffix: Int?): Int = load(testFileSuffix).first().toInt() // simulate response to show test run
 
